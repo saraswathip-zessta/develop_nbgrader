bc41b867c04e886dd79d01c5d8dc737b6b476975
saraswathip-zessta <92719500+saraswathip-zessta@users.noreply.github.com>
2022-10-14 18:52:45 +0530

    Update apihandlers.py

--- nbgrader/server_extensions/formgrader/apihandlers.py
+++ nbgrader/server_extensions/formgrader/apihandlers.py
@@ -4,8 +4,45 @@ import os
 from tornado import web
 
 from .base import BaseApiHandler, check_xsrf, check_notebook_dir
-from ...api import MissingEntry
+from ...api import MissingEntry, Gradebook
 
+
+class CustomExportHandler(BaseApiHandler):
+    @web.authenticated
+    @check_xsrf
+    def get(self):
+        with self.gradebook as gb:
+        grades = []
+
+    # Loop over each assignment in the database
+        for assignment in gb.assignments:
+
+        # Loop over each student in the database
+            for student in gb.students:
+
+                # Create a dictionary that will store information about this student's
+                # submitted assignment
+                score = {}
+                score['max_score'] = assignment.max_score
+                score['student'] = student.id
+                score['assignment'] = assignment.name
+
+                # Try to find the submission in the database. If it doesn't exist, the
+                # `MissingEntry` exception will be raised, which means the student
+                # didn't submit anything, so we assign them a score of zero.
+                try:
+                    submission = gb.find_submission(assignment.name, student.id)
+                except MissingEntry:
+                    score['score'] = 0.0
+                else:
+                    score['score'] = submission.score
+
+                grades.append(score)
+
+        # Create a pandas dataframe with our grade information, and save it to disk
+        grades = pd.DataFrame(grades).set_index(['student', 'assignment'])
+#         grades.to_csv('grades.csv')
+        self.write(json.dumps(grades))
 
 class StatusHandler(BaseApiHandler):
     @web.authenticated
