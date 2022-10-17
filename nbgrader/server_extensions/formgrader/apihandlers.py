b913ac12de1adabf1a87316b775a0ae5d87fcdc8
saraswathip-zessta <92719500+saraswathip-zessta@users.noreply.github.com>
2022-10-17 11:43:58 +0530

    Update apihandlers.py

--- nbgrader/server_extensions/formgrader/apihandlers.py
+++ nbgrader/server_extensions/formgrader/apihandlers.py
@@ -12,37 +12,37 @@ class CustomExportHandler(BaseApiHandler):
     @check_xsrf
     def get(self):
         with self.gradebook as gb:
-        grades = []
+            grades = []
 
     # Loop over each assignment in the database
-        for assignment in gb.assignments:
+            for assignment in gb.assignments:
 
         # Loop over each student in the database
-            for student in gb.students:
+                for student in gb.students:
 
                 # Create a dictionary that will store information about this student's
                 # submitted assignment
-                score = {}
-                score['max_score'] = assignment.max_score
-                score['student'] = student.id
-                score['assignment'] = assignment.name
+                    score = {}
+                    score['max_score'] = assignment.max_score
+                    score['student'] = student.id
+                    score['assignment'] = assignment.name
 
                 # Try to find the submission in the database. If it doesn't exist, the
                 # `MissingEntry` exception will be raised, which means the student
                 # didn't submit anything, so we assign them a score of zero.
-                try:
-                    submission = gb.find_submission(assignment.name, student.id)
-                except MissingEntry:
-                    score['score'] = 0.0
-                else:
-                    score['score'] = submission.score
+                    try:
+                        submission = gb.find_submission(assignment.name, student.id)
+                    except MissingEntry:
+                        score['score'] = 0.0
+                    else:
+                        score['score'] = submission.score
 
-                grades.append(score)
+                    grades.append(score)
 
         # Create a pandas dataframe with our grade information, and save it to disk
-        grades = pd.DataFrame(grades).set_index(['student', 'assignment'])
-#         grades.to_csv('grades.csv')
-        self.write(json.dumps(grades))
+            grades = pd.DataFrame(grades).set_index(['student', 'assignment'])
+#           grades.to_csv('grades.csv')
+            self.write(json.dumps(grades))
 
 class StatusHandler(BaseApiHandler):
     @web.authenticated
