3702f12d503eb01108f808d4efe1b1bf19383ad5
saraswathip-zessta <92719500+saraswathip-zessta@users.noreply.github.com>
2022-12-15 11:41:05 +0530

    Update apihandlers.py

--- nbgrader/server_extensions/formgrader/apihandlers.py
+++ nbgrader/server_extensions/formgrader/apihandlers.py
@@ -12,7 +12,6 @@ import boto3
 import datetime
 from io import StringIO  
 import requests
-from ...auth.jupyterhub import get_jupyterhub_api_url
 
 class StatusHandler(BaseApiHandler):
     @web.authenticated
@@ -481,18 +480,10 @@ class CustomExportHandler(BaseApiHandler):
         except:
             pass
 
-class HubApiUrlHandler(BaseApiHandler):
-    def get(self):
-        try:
-            self.write(json.dumps({'hub_url':get_jupyterhub_api_url()}))
-        except:
-            pass
-            
 default_handlers = [
     (r"/formgrader/api/status", StatusHandler),
     (r"/formgrader/api/customexport",CustomExportHandler),
     (r"/formgrader/api/changecourse/([^/]+)",ChangeCourseHandler),
-    (r"/formgrader/api/gethubapiurl",HubApiUrlHandler),
     (r"/formgrader/api/assignments", AssignmentCollectionHandler),
     (r"/formgrader/api/assignment/([^/]+)", AssignmentHandler),
     (r"/formgrader/api/assignment/([^/]+)/assign", AssignHandler),
